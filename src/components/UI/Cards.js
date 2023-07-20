dfa25a0b898222b174a0d75f1d279c52bb67f0ff
shivam <shivamsaxena251@gmail.com>
2023-07-20 16:21:43 +0530

    first commit

---
+++ src/components/UI/Cards.js
@@ -0,0 +1,7 @@
+import './Cards.css';
+export default function card(props){
+    const classes= 'card '+ props.className;
+    return (
+        <div className={classes}>{props.children}</div>
+    )
+}
---
+++ src/components/expense/Expense.js
@@ -0,0 +1,31 @@
+import "./expenses.css";
+import Expensedata from './ExpenseItem';
+import Card from '../UI/Cards';
+function Expense(props) {
+  return (
+    <Card className="expenses">
+      <Expensedata
+        title={props.data[0].title}
+        amount={props.data[0].amount}
+        date={props.data[0].date}
+      ></Expensedata>
+       <Expensedata
+        title={props.data[1].title}
+        amount={props.data[1].amount}
+        date={props.data[1].date}
+      ></Expensedata>
+       <Expensedata
+        title={props.data[2].title}
+        amount={props.data[2].amount}
+        date={props.data[2].date}
+      ></Expensedata>
+       <Expensedata
+        title={props.data[3].title}
+        amount={props.data[3].amount}
+        date={props.data[3].date}
+      ></Expensedata>
+      
+    </Card>
+  );
+}
+export default Expense;
