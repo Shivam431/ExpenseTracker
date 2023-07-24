b4181daf981159d61b4f6e78aece22238264cf86
shivam <shivamsaxena251@gmail.com>
2023-07-24 20:18:38 +0530

    added changes for filter year,chart,conditional logic

--- src/App.js
+++ src/App.js
@@ -1,35 +1,43 @@
-import Expense from './components/expense/Expense';
-import NewExpense from './components/NewExpense/NewExpense';
+import { useState } from "react";
+import Expense from "./components/expense/Expense";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const INIT_EXPENSE = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
+  const [expenses, setExpenses] = useState(INIT_EXPENSE);
+  const saveNewExpense = (data) => {
+     setExpenses((previousExpense) => {
+     return [data, ...previousExpense];
+    });
+  };
   return (
     <div>
-      <NewExpense />
-      <Expense data={expenses}/>
+      <NewExpense onSaveNewExpense={saveNewExpense} />
+      <Expense data={expenses} />
     </div>
   );
 }
 
 export default App;
+//always use key attribute when mapping array
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const [isAdd, setAdd] = React.useState(false);
+  const saveExpenseData = (ExpenseData) => {
+    const expenseData = {
+      ...ExpenseData,
+      id: Math.random().toString(),
+    };
+    props.onSaveNewExpense(expenseData);
+    setAdd(false);
+  };
+
+  const clickListner = () => {
+    setAdd(true);
+  };
+
+  const stopAdding = () => {
+    setAdd(false);
+  };
   return (
-    <div className='new-expense'>
-      <ExpenseForm />
+    <div className="new-expense">
+      {!isAdd && <button onClick={clickListner}>Add New Expense</button>}
+      {isAdd && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseData}
+          onCancel={stopAdding}
+        />
+      )}
     </div>
   );
 };
